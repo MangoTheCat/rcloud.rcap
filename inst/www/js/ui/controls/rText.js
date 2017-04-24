3972c847bec8fbbacb8abfaf6c14ba051d87c2d4
Shane Porter <shane@freshclickmedia.com>
2017-04-24 16:19:50 +0100

    rtext

--- inst/www/js/ui/controls/rText.js
+++ inst/www/js/ui/controls/rText.js
@@ -1,9 +1,10 @@
 define(['rcap/js/ui/controls/gridControl',
     'rcap/js/ui/properties/textProperty',
     'rcap/js/ui/properties/autocompleteProperty',
+    'rcap/js/ui/properties/dropdownProperty',
     'text!controlTemplates/rText.tpl',
     'text!controlTemplates/rText-design.tpl'
-], function(GridControl, TextProperty, AutocompleteProperty, tpl, dtpl) {
+], function(GridControl, TextProperty, AutocompleteProperty, DropdownProperty, tpl, dtpl) {
 
     'use strict';
 
@@ -20,6 +21,24 @@ define(['rcap/js/ui/controls/gridControl',
                         label: 'R Function',
                         helpText: 'R Function for this control.',
                         isRequired: true
+                    }),
+                    new TextProperty({
+                      uid: 'linkUrl',
+                      label : 'Link URL',
+                      defaultValue : '',
+                      helpText : 'The URL where you go if you click on the image',
+                      isRequired: false
+                    }),
+                    new DropdownProperty({
+                        uid: 'linkUrlTarget',
+                        label: 'Link target',
+                        helpText: 'Should the link open in the current tab or a new tab.',
+                        isRequired: false,
+                        defaultOptionText: 'Current',
+                        availableOptions: [{
+                            text: 'New',
+                            value: 'new'
+                        }]
                     })
                 ]
             });
--- inst/www/js/ui/controls/templates/rText.tpl
+++ inst/www/js/ui/controls/templates/rText.tpl
@@ -1,4 +1,9 @@
+<% if(control.getPropertyValue('linkUrl')) { %>
+  <a href="<%=control.getPropertyValue('linkUrl')%>" <%= control.getPropertyValue('linkUrlTarget') === 'new' ? ' target="_blank"' : '' %> class="full">
+<% } %>
+
 <div id="<%=control.id%>" class="rtext" data-controltype="rtext">Please wait while the content is loaded...</div>
-<%if(designTimeDescription.length && isDesignTime) {%>
-	<pre><%=designTimeDescription%></pre>
-<%}%>
+
+<% if(control.getPropertyValue('linkUrl')) { %>
+  </a>
+<% } %>
